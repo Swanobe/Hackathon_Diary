e4ea5ebdbf373b940a02aa56b80b9fd3558ad7b0
Sean <sean_obeirne@outlook.com>
2023-05-04 15:23:13 +0100

    Javascript edited to make both text boxes log the entry when submit clicked, Problem with layout when more than one entry is submitted

--- client/index.js
+++ client/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 // My Starter JavaScript Template
 
 // Journal Entry Form
@@ -12,59 +10,59 @@ const getEntryTitle = document.getElementsByClassName(`entry-text-title`);
 const getEntryText = document.getElementsByClassName(`entry-text-box`);
 
 function addEntryToDom(event) {
-        event.preventDefault();
-        const d = new Date();
-        const month = new Array();
-        month[0] = 'January';
-        month[1] = 'February';
-        month[2] = 'March';
-        month[3] = 'April';
-        month[4] = 'May';
-        month[5] = 'June';
-        month[6] = 'July';
-        month[7] = 'August';
-        month[8] = 'September';
-        month[9] = 'October';
-        month[10] = 'November';
-        month[11] = 'December';
-        const n = month[d.getMonth()];
-        const day = d.getDay();
-        const year = d.getFullYear();
+  event.preventDefault();
+  const d = new Date();
+  const month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  const n = month[d.getMonth()];
+  const day = d.getDay();
+  const year = d.getFullYear();
 
+  const heading = document.createElement(`h2`);
+  heading.className = `heading-results`;
+  heading.textContent = `Retro Entries`;
+  entryResultRow.insertAdjacentElement(`beforebegin`, heading);
 
-        const heading = document.createElement(`h2`);
-        heading.className = `heading-results`;
-        heading.textContent = `Journal Entries`;
-        entryResultRow.insertAdjacentElement(`beforebegin`, heading)
+  // Adding Div
+  const entryDiv = document.createElement(`div`);
+  entryDiv.className = `single-entry-div`;
+  entryResultRow.appendChild(entryDiv);
 
-        // Adding Div
-        const entryDiv = document.createElement(`div`);
-        entryDiv.className = `single-entry-div`;
-        entryResultRow.appendChild(entryDiv);
+  // Adding Div Element h3
+  const entryHeading = document.createElement(`h3`);
+  entryHeading.className = `single-entry-heading`;
+  entryHeading.textContent = getEntryTitle[0].value;
+  entryDiv.appendChild(entryHeading);
 
-        // Adding Div Element h3
-        const entryHeading = document.createElement(`h3`);
-        entryHeading.className = `single-entry-heading`;
-        entryHeading.textContent = getEntryTitle[0].value;
-        entryDiv.appendChild(entryHeading);
+  // Adding Div Element Date
 
-        // Adding Div Element Date
+  const entryDate = document.createElement(`p`);
+  entryDate.className = `single-entry-date`;
+  entryDate.textContent = `Date Added: ${day} ${n} ${year}`;
+  entryDiv.appendChild(entryDate);
 
-        const entryDate = document.createElement(`p`);
-        entryDate.className = `single-entry-date`;
-        // eslint-disable-next-line no-cond-assign
-        if ((getEntryTitle[0].value = getEntryTitle[0].value)) {
-                entryDate.textContent = `Date Added: ${day} ${n} ${year}`;
-                entryDiv.appendChild(entryDate);
-        }
+  // Adding Div Element paragraph for first text area
+  const entryParagraph1 = document.createElement(`p`);
+  entryParagraph1.className = `single-entry-text`;
+  entryParagraph1.textContent = getEntryText[0].value;
+  entryDiv.appendChild(entryParagraph1);
 
-        // Adding Div Element paragraph
-
-        const entryParagraph = document.createElement(`p`);
-        entryParagraph.className = `single-entry-text`;
-        entryParagraph.textContent = getEntryText[0].value;
-        entryDiv.appendChild(entryParagraph);
-        getEntryText[0].value = ``;
+  // Adding Div Element paragraph for second text area
+  const entryParagraph2 = document.createElement(`p`);
+  entryParagraph2.className = `single-entry-text`;
+  entryParagraph2.textContent = getEntryText[1].value;
+  entryDiv.appendChild(entryParagraph2);
 }
 
 entryForm.addEventListener(`submit`, addEntryToDom);
